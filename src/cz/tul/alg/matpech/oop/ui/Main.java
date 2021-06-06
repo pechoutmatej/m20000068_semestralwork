a34d942c6257ceba98a1278ab2dc864e6a77dc21
pechoutmatej <73175139+pechoutmatej@users.noreply.github.com>
2021-06-06 21:09:35 +0200

    InputMismatch fix

--- src/cz/tul/alg/matpech/oop/ui/Main.java
+++ src/cz/tul/alg/matpech/oop/ui/Main.java
@@ -6,6 +6,7 @@ import cz.tul.alg.matpech.oop.model.Logic;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -18,12 +19,12 @@ public class Main {
         StringBuilder str = new StringBuilder();
         String email;
         String resultFile;
-        int compChoice;
+        String compChoice;
         //if vars
         int fuelChoice;
         double fuelPrice;
         //přídávání auta vars
-        int choice;
+        String choice;
         String manufacturer;
         String model;
         Double consumption;
@@ -46,8 +47,8 @@ public class Main {
             System.out.println("8. Porovnávání zadaných aut");
             System.out.println("9. konec");
             System.out.println("---------------------");
-            choice = sc.nextInt();
-            if(choice == 1)
+            choice = sc.next();
+            if(choice.equals("1"))
             {
                 System.out.println("Zadej .csv soubor s auty :");
                 try {
@@ -67,33 +68,46 @@ public class Main {
                 }
                 System.out.println("---------------------");
             }
-            else if(choice == 2)
+            else if(choice.equals("2"))
             {
-                System.out.println("Zadej výrobce vozu");
-                manufacturer = sc.next();
-                System.out.println("Zadej model vozu");
-                model = sc.next();
-                System.out.println("Zadej spotřebu paliva na 100km");
-                consumption = sc.nextDouble();
-                System.out.println("Zadej velikost nádrže v litrech");
-                fuelTankSize = sc.nextDouble();
-                System.out.println("Zadej náklady na jeden kilometr vyjma PHM(amortizace, servis,...)");
-                kmCosts = sc.nextDouble();
-                System.out.println("Zadej palivo");
-                System.out.println("(0 - benzin | 1 - nafta | 2 - ethanol | 3 - benzin100 | 4 - LPG)");
-                fuelType = sc.nextInt();
-                System.out.println("Zadej SPZ");
-                spz = sc.next();
-                car = new Car(manufacturer, model, consumption, fuelTankSize, kmCosts, fuelType, spz);
-                logic.addCar(car);
+                try {
+                    System.out.println("Zadej výrobce vozu");
+                    manufacturer = sc.next();
+                    System.out.println("Zadej model vozu");
+                    model = sc.next();
+                    System.out.println("Zadej spotřebu paliva na 100km");
+                    consumption = sc.nextDouble();
+                    System.out.println("Zadej velikost nádrže v litrech");
+                    fuelTankSize = sc.nextDouble();
+                    System.out.println("Zadej náklady na jeden kilometr vyjma PHM(amortizace, servis,...)");
+                    kmCosts = sc.nextDouble();
+                    System.out.println("Zadej palivo");
+                    System.out.println("(0 - benzin | 1 - nafta | 2 - ethanol | 3 - benzin100 | 4 - LPG)");
+                    fuelType = sc.nextInt();
+                    System.out.println("Zadej SPZ");
+                    spz = sc.next();
+                    car = new Car(manufacturer, model, consumption, fuelTankSize, kmCosts, fuelType, spz);
+                    logic.addCar(car);
+                }
+                catch(InputMismatchException ex)
+                {
+                    sc.nextLine();
+                    System.out.println("Špatný vstup");
+                }
                 System.out.println("---------------------");
             }
-            else if(choice == 3)
+            else if(choice.equals("3"))
             {
-                System.out.println(logic.carsToString());
-                System.out.println("---------------------");
+                if(logic.carsToString().length() == 0)
+                {
+                    System.out.println("Ještě nejsou načtená žádná auta");
+                }
+                else {
+                    System.out.println(logic.carsToString());
+                    System.out.println("---------------------");
+                }
             }
-            else if(choice == 4)
+            else if(choice.equals("4"))
             {
                 if(logic.pricesToString().equals("[0.0, 0.0, 0.0, 0.0, 0.0]"))
                 {
@@ -110,7 +124,7 @@ public class Main {
                 }
                 System.out.println("---------------------");
             }
-            else if(choice == 5)
+            else if(choice.equals("5"))
             {
                 if(logic.carsToString().equals("") || logic.pricesToString().equals("[0.0, 0.0, 0.0, 0.0, 0.0]"))
                 {
@@ -124,62 +138,92 @@ public class Main {
                 }
                 System.out.println("---------------------");
             }
-            else if(choice == 6)
+            else if(choice.equals("6"))
             {
-                System.out.println("Zadej palivo jehož cenu chceš změnit");
-                System.out.println("(0 - benzin | 1 - nafta | 2 - ethanol | 3 - benzin100 | 4 - LPG)");
-                fuelChoice = sc.nextInt();
-                System.out.println("Aktuální cena zvoleného paliva");
-                System.out.println(logic.getPricesIndex(fuelChoice));
-                System.out.println("Zadej novou cenu");
-                fuelPrice = sc.nextDouble();
-                logic.changePrices(fuelChoice, fuelPrice);
+                if(logic.pricesToString().equals("[0.0, 0.0, 0.0, 0.0, 0.0]"))
+                {
+                    System.out.println("Ještě nejsou zadány ceny paliv");
+                }
+                else {
+                    System.out.println("Zadej palivo jehož cenu chceš změnit");
+                    System.out.println("(0 - benzin | 1 - nafta | 2 - ethanol | 3 - benzin100 | 4 - LPG)");
+                    fuelChoice = sc.nextInt();
+                    if (fuelChoice < 0 || fuelChoice > 4) {
+                        System.out.println("Neplatný index paliva");
+                    } else {
+                        System.out.println("Aktuální cena zvoleného paliva");
+                        System.out.println(logic.getPricesIndex(fuelChoice) + " Kč/L");
+                        System.out.println("Zadej novou cenu");
+                        try {
+                            fuelPrice = sc.nextDouble();
+                            logic.changePrices(fuelChoice, fuelPrice);
+                        } catch (InputMismatchException ex) {
+                            sc.nextLine();
+                            System.out.println("Špatný vstup");
+                        }
+                    }
+                }
                 System.out.println("---------------------");
             }
-            else if(choice == 7)
+            else if(choice.equals("7"))
             {
-                System.out.println("Zadej výrobce vozu");
-                manufacturer = sc.next();
-                System.out.println("Zadej model vozu");
-                model = sc.next();
-                System.out.println("Zadej spotřebu paliva na 100km");
-                consumption = sc.nextDouble();
-                System.out.println("Zadej velikost nádrže v litrech");
-                fuelTankSize = sc.nextDouble();
-                System.out.println("Zadej náklady na jeden kilometr vyjma PHM(amortizace, servis,...)");
-                kmCosts = sc.nextDouble();
-                System.out.println("Zadej palivo");
-                System.out.println("(0 - benzin | 1 - nafta | 2 - ethanol | 3 - benzin100 | 4 - LPG)");
-                fuelType = sc.nextInt();
-                System.out.println("Zadej SPZ");
-                spz = sc.next();
-                car = new Car(manufacturer, model, consumption, fuelTankSize, kmCosts, fuelType, spz);
-                System.out.println("Zadej email pro zaslání reportu");
-                email = sc.next();
-                logic.sendMail(email, car);
+                try {
+                    System.out.println("Zadej výrobce vozu");
+                    manufacturer = sc.next();
+                    System.out.println("Zadej model vozu");
+                    model = sc.next();
+                    System.out.println("Zadej spotřebu paliva na 100km");
+                    consumption = sc.nextDouble();
+                    System.out.println("Zadej velikost nádrže v litrech");
+                    fuelTankSize = sc.nextDouble();
+                    System.out.println("Zadej náklady na jeden kilometr vyjma PHM(amortizace, servis,...)");
+                    kmCosts = sc.nextDouble();
+                    System.out.println("Zadej palivo");
+                    System.out.println("(0 - benzin | 1 - nafta | 2 - ethanol | 3 - benzin100 | 4 - LPG)");
+                    fuelType = sc.nextInt();
+                    System.out.println("Zadej SPZ");
+                    spz = sc.next();
+                    car = new Car(manufacturer, model, consumption, fuelTankSize, kmCosts, fuelType, spz);
+                    System.out.println("Zadej email pro zaslání reportu");
+                    email = sc.next();
+                    logic.sendMail(email, car);
+                }
+                catch(InputMismatchException ex)
+                {
+                    sc.nextLine();
+                    System.out.println("Špatný vstup");
+                }
                 System.out.println("---------------------");
             }
-            else if(choice == 8)
+            else if(choice.equals("8"))
             {
                 if(logic.carsToString().equals(""))
                 {
                     System.out.println("Nekompletní data k výpočtu");
                 }
-                else {
+                else
+                {
                     System.out.println("0 - Dle ceny na 1km | 1 - dle dojezdu na plnou nádrž");
-                    compChoice = sc.nextInt();
+                    compChoice = sc.next();
                     logic.calculateAll();
-                    switch (compChoice) {
-                        case 0:
+
+                        if(compChoice.equals("0")) {
                             System.out.println(logic.sortByPrice());
-                            break;
-                        case 1:
+                        }
+                        else if(compChoice.equals("1"))
+                        {
                             System.out.println(logic.sortByRange());
-                            break;
-                    }
+                        }
+                        else
+                        {
+                            System.out.println("Špatný vstup");
+                        }
                 }
+
+
+
             }
-            else if(choice == 9)
+            else if(choice.equals("9"))
             {
                 System.out.println("Konec programu");
                 cond = false;
